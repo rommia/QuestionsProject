74ee0f2f4fd0c20fe9d3dfffad0cbd5848cc6470
Romina <rommia_@hotmail.com>
2020-08-24 20:35:45 -0300

    answers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :answers
   get 'welcome/index'
-  resources :questions
+  resources :questions do
+    resources :answers
+    end
   root 'questions#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_16_174943) do
+ActiveRecord::Schema.define(version: 2020_08_24_214511) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string "content"
+    t.integer "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
@@ -19,4 +27,5 @@ ActiveRecord::Schema.define(version: 2020_08_16_174943) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "answers", "questions"
 end
